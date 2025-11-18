4edd04b724eab99e0e2e72fe8d245f88f99085af
LeveL-P1 <pawanbanait.dev@gmail.com>
2025-11-18 23:52:36 +0530

    removed role based authentication, removed proxy/middleware

--- src/lib/auth/requireAdmin.ts
+++
@@ -1,7 +0,0 @@
-import { requireRole } from '@/lib/auth/guards'
-import { UserRole } from '@/types/globals'
-import { ClerkProvider } from '@clerk/nextjs'
-
-export async function requireAdmin() {
-  return requireRole('ADMIN' as UserRole)
-}
